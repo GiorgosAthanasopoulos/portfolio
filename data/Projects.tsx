9632eea798c46a2b167efeb6bc162d61879a79b3
GiorgosAthanasopoulos <spzdoom@gmail.com>
2023-02-17 18:36:21 +0200

    data update

--- data/Projects.tsx
+++ data/Projects.tsx
@@ -3,16 +3,6 @@ import Project from "@/types/Project";
 //TODO: fill projects
 const Projects: Project[] = [
 	{
-		title: "Project 2",
-		tech: ["Tech 4", "Tech 5", "Tech 6"],
-		image: "/public/project2.png",
-	},
-	{
-		title: "Project 3",
-		tech: ["Tech 7", "Tech 8", "Tech 9"],
-		image: "/public/project3.png",
-	},
-	{
 		title: "Developer Portfolio",
 		tech: ["Next.js", "React", "Tailwind"],
 		image: "/portfolio.png",
